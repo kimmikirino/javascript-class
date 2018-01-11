7ca52e8400b6a33369c06c987211a835cd8fbb88
JAQUELINE KIMMI KIRINO <34568531810@jpmorgan.sp>
2018-01-11 21:55:33 -0200

    Addin shipping info

--- alta-fidelidade.js
+++ alta-fidelidade.js
@@ -1,7 +1,11 @@
 var products = [
     {
         id: 1,
-        image: 'images/1.jpg',
+        image: {
+            src: 'images/1.jpg',
+            alt: 'Camisa 1',
+            title: 'Camisa bonita'
+        },
         name: 'Pulma Golf',
         color: '#2f52b7',
         size: 'XXL',
@@ -10,7 +14,11 @@ var products = [
     },
     {
         id: 2,
-        image: 'images/2.jpg',
+        image:  {
+            src: 'images/2.jpg',
+            alt: 'Camisa 2',
+            title: 'Camisa azur'
+        },
         name: 'Nike Mens running',
         color: '#007698',
         size: '43',
@@ -18,17 +26,35 @@ var products = [
         qty: 1
     },
     {
+        id: 4,
+        image:  {
+            src: 'images/3.jpg',
+            alt: 'Camisa 3',
+            title: 'Camisa tb azur'
+        },
+        name: 'DC Mens Axis',
+        color: '#f9c0f9',
+        size: 'S',
+        price: '10',
+        qty: 10
+    },
+    {
         id: 3,
-        image: 'images/3.jpg',
+        image:  {
+            src: 'images/4.png',
+            alt: 'Camisa 4',
+            title: 'Camisa rosa'
+        },
         name: 'DC Mens Axis',
         color: '#1c376a',
         size: 'S',
         price: '89',
         qty: 2
-    }
+    },
+
 ];
 
-function criarElemento (tag, texto, classe, append) {
+function criarElemento(tag, texto, classe, append) {
     var criaElemento = document.createElement(tag);
     var elementoTexto = document.createTextNode(texto);
     criaElemento.className += classe;
@@ -38,40 +64,30 @@ function criarElemento (tag, texto, classe, append) {
 }
 
 var tbodyProduct = document.getElementById('products');
-var bodyProduct = ''; 
+var bodyProduct = '';
 
 function list() {
     //tbodyProduct.innerHTML = "";
-    bodyProduct = "";   
+    bodyProduct = "";
+    document.getElementById('shoppingQtyTotal').innerHTML = `You have <b>${products.length} items</b> in your order`;
     let subtotal = 0;
-    for(var i=0; i < products.length; i++) {
+    for (var i = 0; i < products.length; i++) {
         var img = new Image();
         let total = 0;
-        //var element = document.createElement("tr");
-        //tbodyProduct.appendChild(criarElemento('td', 'x', '', element));
-        // img.src = products[i].image;
-        // img.className = 'img-red';
-        // criarElemento('td', products[i].name, '', element);
-        // tbodyProduct.innerHTML += '<td><img src="' + products[i].image + '" class="img-red" />';
-        // tbodyProduct.innerHTML += products[i].name + '</td>';
-        //tdImage.appendChild(img);
-        // criarElemento('td', products[i].name, '', element);
-        // criarElemento('td', products[i].price, '', element);
-        // criarElemento('td', products[i].qty, '', element);
-        // criarElemento('td', products[i].total, '', element);
+
         total = Number(products[i].price) * Number(products[i].qty);
-        bodyProduct += '<tr><td class="align-middle"><span class="delete-item">x</span></td>' ;
-        bodyProduct += '<td class="align-middle"><div class="box"><img src="'+ products[i].image + '" class="img-red" />' ;
+        bodyProduct += '<tr><td class="align-middle"><span class="delete-item" onclick="remove(' + products[i].id + ')">x</span></td>';
+        bodyProduct += '<td class="align-middle"><div class="box"><img src="' + products[i].image.src + '" class="img-red" alt="' + products[i].image.alt + '"/>';
         bodyProduct += '<div class="name-box"><b>' + products[i].name;
-        bodyProduct += '</b><div class="name-box__ball"> cor: <div class="ball" style="background-color:' + products[i].color + '"></div>';
-        bodyProduct += ' Tamanho: <span class="name-box__size">' + products[i].size + '</span></div></div></td>';
-        bodyProduct += '<td class="align-middle">$<b>'+ products[i].price +'</b></td>' ;
-        bodyProduct += '<td class="align-middle"><div class="qty"><div class="qty__left" onclick="dec('+products[i].id+')">-</div><div class="qty__middle">'+ products[i].qty +'</div><div class="qty__right" onclick="inc('+products[i].id+')">+</div></div></td>' ;
-        bodyProduct += '<td class="align-middle">$<b>'+ total +'</b></td>' ;
+        bodyProduct += '</b><div class="name-box__ball"> Color: <div class="ball" style="background-color:' + products[i].color + '"></div>';
+        bodyProduct += ' Size: <span class="name-box__size">' + products[i].size + '</span></div></div></td>';
+        bodyProduct += '<td class="align-middle">$<b>' + products[i].price + '</b></td>';
+        bodyProduct += '<td class="align-middle"><div class="qty"><div class="qty__left" onclick="dec(' + products[i].id + ')">-</div><div class="qty__middle">' + products[i].qty + '</div><div class="qty__right" onclick="inc(' + products[i].id + ')">+</div></div></td>';
+        bodyProduct += '<td class="align-middle">$<b>' + total + '</b></td>';
         bodyProduct += '</tr>';
         subtotal += Number(total);
     }
-    
+
     bodyProduct += '<tr><td colspan="5" class="text-right">Subtotal: <h4 class="text-inline">$<b>' + subtotal + '</b></h4></td></tr>';
     tbodyProduct.innerHTML = bodyProduct;
     // console.log(`texto string linha 1
@@ -80,10 +96,10 @@ function list() {
 }
 
 function productFilter(id) {
-    return products.filter(function(product){
-        if(product.id === id){
+    return products.filter(function (product) {
+        if (product.id === id) {
             return product;
-        } 
+        }
     })[0];
 };
 
@@ -95,10 +111,40 @@ function inc(id) {
 
 function dec(id) {
     var productFiltered = productFilter(id);
-    if(productFiltered.qty) {
+    if (productFiltered.qty) {
         productFiltered.qty--;
     }
     list();
 }
+
+function remove(id) {
+    products = products.filter(function (product) {
+        return product.id !== id;
+    });
+    list();
+}
+
+function changeTab(event, tabName, linkTab) {
+    console.log(tabName);
+    // Declare all variables
+    var i, tabcontent, tablinks;
+    
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tab-pane");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+    
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("nav-link");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+    
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(tabName).style.display = "block";
+        document.getElementById(linkTab).className += " active";
+}
+
 
 list();
